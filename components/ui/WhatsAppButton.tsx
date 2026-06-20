77862e401f2c498bda234de956c046ce545beafa
Gouranga Ghosh <88451025+gourangabwn98@users.noreply.github.com>
2026-06-20 14:57:19 +0530

    ch

--- components/ui/WhatsAppButton.tsx
+++ components/ui/WhatsAppButton.tsx
@@ -5,7 +5,7 @@ import { MessageCircle } from "lucide-react";
 import { photographer } from "@/data";
 
 export default function WhatsAppButton() {
-  const url = `https://wa.me/${photographer.whatsapp}?text=Hi%20Elara%2C%20I%27d%20love%20to%20discuss%20a%20photography%20session.`;
+  const url = `https://wa.me/${photographer.whatsapp}?text=Hi%20RK%2C%20I%27d%20love%20to%20discuss%20a%20photography%20session.`;
   return (
     <motion.a
       href={url}
